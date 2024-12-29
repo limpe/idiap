94da750987988afdb64e2702326e8baace9961ef
limpe <limpebro@gmail.com>
2024-12-29 08:26:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ async def process_image_with_pixtral_multiple(image_path: str, repetitions: int 
             }
 
             data = {
-                "model": "mistral-large-latest",
+                "model": "ministral-8b-latest",
                 "messages": [
                     {
                         "role": "user",
