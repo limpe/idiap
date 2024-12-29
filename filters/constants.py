50418c7991a3b3f422af7fc5058fea0ebf6d91eb
limpe <limpebro@gmail.com>
2024-12-30 00:46:41 +0700

    Update constants.py

--- filters/constants.py
+++ filters/constants.py
@@ -1,5 +1,3 @@
-"""
-Konstanta untuk package filters
-"""
+from typing import Dict
 MAX_LINE_LENGTH = 4096  # Maximum line length for Telegram messages
 DEFAULT_LANGUAGE = 'id'  # Default language for text processing
