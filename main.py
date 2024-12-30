361e777218290401c97d0c137b5964814b6467ce
limpe <limpebro@gmail.com>
2024-12-30 13:59:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -328,7 +328,7 @@ async def process_with_mistral(messages: List[Dict[str, str]]) -> Optional[str]:
     messages.insert(0, {"role": "system", "content": "Pastikan semua respons diberikan dalam Bahasa Indonesia Yang Mudah Di pahami."})
 
     data = {
-        "model": "pixtral-large-latest",
+        "model": "open-mistral-nemo",
         "messages": messages,
         "max_tokens": 10000
     }
