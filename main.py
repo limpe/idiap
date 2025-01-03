316a65fc8e07f98b14725aa814e98ae27f8e01e5
limpe <limpebro@gmail.com>
2025-01-04 03:30:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ from typing import List, Dict
 from typing import Union, Tuple
 
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
-gemini_model = genai.GenerativeModel("gemini-2.0-flash-exp")
+gemini_model = genai.GenerativeModel("learnlm-1.5-pro-experimental")
 
 
 # Konstanta untuk batasan ukuran file
