d010d0751c506f79e221cc5c3dbd75fcd6628337
limpe <limpebro@gmail.com>
2025-01-11 11:00:43 +0700

    Update keywords.py

--- keywords.py
+++ keywords.py
@@ -1,5 +1,5 @@
 complex_keywords = [
-    "jelaskan", "analisis", "detail", "mendalam", "komprehensif", 
+    "jelaskan", "analisis", "detail", "detil", "mendalam", "komprehensif", 
     "bagaimana cara", "mengapa", "apa alasan", "faktor", "dampak", 
     "perbandingan", "proses", "strategi", "solusi", "rekomendasi"
     "analisa",
